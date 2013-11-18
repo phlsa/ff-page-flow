b32a97f9c5d59efdc2e482071d289d39bfd9c50c
Philipp Sackl <philippsackl@gmail.com>
2013-11-18 14:24:45 -0800

    add three amazing types of load schemes

--- js/app.js
+++ js/app.js
@@ -5,10 +5,30 @@ $( document ).ready( function() {
 	$( '.page1' ).click(function() {
 		$('.page1').addClass('half-out-left');
 		$('.page2').addClass('half-in-right');
-		_.delay(function() {
-			$('.page1').removeClass('half-out-left').addClass('out-left');
-			$('.page2').removeClass('half-in-right out-right');
-		}, 3000);
+		$('.small-spinner').show();
+
+		if ( transitionType === 'type-4' ) {
+			_.delay(function() {
+				$('.page1').removeClass('half-out-left').addClass('out-left');
+				_.delay(function() {
+					$('.page2').removeClass('half-in-right out-right');
+					$('.page2').css({'height': '100px'});
+					_.delay(function() {
+						$('.page2').css({'height': '400px'});
+						_.delay(function() {
+							$('.page2').css({'height': '616px'});
+							$('.small-spinner').hide();
+						}, 500);
+					}, 500);
+				}, 1000);
+			}, 1000);
+		} else {
+			_.delay(function() {
+				$('.page1').removeClass('half-out-left').addClass('out-left');
+				$('.page2').removeClass('half-in-right out-right');
+				$('.small-spinner').hide();
+			}, 3000);
+		}
 	});
 
 	$( '.back' ).click(function() {
